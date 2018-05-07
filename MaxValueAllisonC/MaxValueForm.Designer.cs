c79d1cbb07e6e9829998eb3caa6cd34a17b05bcf
Allison <allison.cook@ocsbstudent.ca>
2018-05-07 13:08:40 -0400

    Initial commit

---
+++ MaxValueAllisonC/MaxValueForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MaxValueAllisonC
+{
+    partial class frmMaxValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(121, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 29);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 19;
+            this.lstNumbers.Location = new System.Drawing.Point(98, 47);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(122, 175);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(75, 242);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(121, 19);
+            this.lblMax.TabIndex = 2;
+            this.lblMax.Text = "The max value is:";
+            // 
+            // frmMaxValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(320, 282);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMaxValue";
+            this.Text = "Finding the max value by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMax;
+    }
+}
+
---
+++ MaxValueAllisonC/MaxValueForm.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 7 May, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #35 - Find Max Value
+ * This program finds the max value out of ten randomly generated numbers
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxValueAllisonC
+{
+    public partial class frmMaxValue : Form
+    {
+        public frmMaxValue()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMaxValue(int[] tmpArrayOfNumbers)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declaring local variables and constants
+            const int MAX_RANDOM_NUMBER = 500;
+            const int ARRAY_SIZE = 10;
+            int randomNumber;
+            int counter;
+            int maxValue;
+            Random randomNumberGenerator = new Random();
+
+            //creating an array
+            int[] arrayOfNumbers = new int[ARRAY_SIZE];
+
+            //clearing the list box
+            lstNumbers.Items.Clear();
+
+            //creating random numbers and adding them to the array
+            for (counter = 0; counter < ARRAY_SIZE; counter++)
+            {
+                //generate the random number
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER);
+
+                //assign the random number to the array at the index of counter
+                arrayOfNumbers[counter] = randomNumber;
+
+                //add the random number to the list box
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form to show the updated list box
+                this.Refresh();
+            }
+
+            //calling the function to find max value
+            maxValue = GetMaxValue(arrayOfNumbers);
+
+        }
+    }
+}
